6a9b01377acc9eb56fa229119f0890e880e80e5b
Elco Klingen <disillusion84@gmail.com>
2019-11-23 18:37:46 +0100

    Internal refactoring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 
 # Changelog
 
+v1.0.2 - Internal refactoring
 v1.0.1 - Didn't return errors correctly
 v1.0.0 - Initial release
--- index.js
+++ index.js
@@ -1,2 +1,69 @@
+// Small vinyl-stream wrapper -aka Gulp plugin- for nunjucks.
+//
+// TODO:
+// - Remove dependency on deepmerge
+// - Fix Nunjucks losing file path when macro errors out.
 
-module.exports = require('./src/nunjucks')
+const { resolve } = require('path')
+const { Transform } = require('stream')
+
+const DEFAULT_OPTIONS = {
+  path: '.',
+  ext: '.html',
+  data: {},
+  manageEnv: null,
+  loaders: []
+}
+
+const DEFAULT_ENV_OPTIONS = {
+  autoescape: false,
+  throwOnUndefined: true,
+  trimBlocks: false,
+  lstripBlocks: false,
+  watch: false,
+  noCache: false,
+  useCache: true,
+  async: false,
+  express: null,
+  tags: null
+}
+
+function nunjucksWrapper (options = {}) {
+  const merge = require('deepmerge')
+  const nunjucks = require('nunjucks')
+
+  options = { ...DEFAULT_OPTIONS, ...options }
+  options.envOptions = { ...DEFAULT_ENV_OPTIONS, ...options.envOptions }
+
+  nunjucks.configure(options.envOptions)
+
+  if (!options.loaders || !options.loaders.length) {
+    options.loaders = [new nunjucks.FileSystemLoader(options.path)]
+  }
+
+  function onComplete (error, result, file, callback) {
+    if (error) {
+      return callback(new Error(error, { fileName: file.path }))
+    }
+
+    file.contents = Buffer.from(result)
+
+    return callback(null, file)
+  }
+
+  function transform (file, encoding, callback) {
+    const compiler = new nunjucks.Environment(options.loaders, options.envOptions)
+
+    if (options.manageEnv) {
+      options.manageEnv.call(null, compiler)
+    }
+
+    // TODO: The filepath is wrong in error stack straces (it gives either unknown or the original entrypoint). Not sure how to fix this. Both .renderString() and .render() have the same bug.
+    compiler.renderString(file.contents.toString(), merge(file.data || {}, options.data), { path: resolve(file.path) }, (error, result) => onComplete(error, result, file, callback))
+    // compiler.render(file.path, merge(file.data || {}, options.data), (error, result) => onComplete(error, result, file))
+  }
+
+  return new Transform({ transform, readableObjectMode: true, writableObjectMode: true })
+}
+
+module.exports = nunjucksWrapper
